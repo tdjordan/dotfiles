275d7cfd5af21bcbdef2a970b43f701fc182fa22
Thomas Jordan <thomasjordan@ge.com>
2021-03-08 17:58:40 -0600

    some support for vim8

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -5,7 +5,7 @@ require 'nvim-treesitter.configs'.setup {
   },
   refactor = {
     highlight_definitions = { enable = true },
-    highlight_current_scope = { enable = true },
+    --highlight_current_scope = { enable = true },
   },
   playground = {
     enable          = true,
