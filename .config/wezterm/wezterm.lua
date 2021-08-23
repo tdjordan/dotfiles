b01bba135580d5aee9eb60f9dbe5341f5a0958fb
Tom Jordan <tdjordan@gmail.com>
2021-08-22 19:55:30 -0500

    checkpoint

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wt = require 'wezterm'
+
+return {
+  color_scheme = 'Batman'
+  , font = wt.font 'FiraCode Nerd Font'
+  , font_size = 17.0
+  -- , enable_tab_bar = false
+  , hide_tab_bar_if_only_one_tab = true
+  , window_decorations = 'RESIZE'
+}
