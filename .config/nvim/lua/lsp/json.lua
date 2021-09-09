2a6170e301f0b5e57a93fca16ba63bf984293da4
Tom Jordan <tdjordan@gmail.com>
2021-09-08 23:50:55 -0500

    adjust some lsp configs
    - attempt initial xml config
    - start on capabilities specifications

--- .config/nvim/lua/lsp/json.lua
+++ .config/nvim/lua/lsp/json.lua
@@ -4,7 +4,11 @@
 --
 --require 'lspconfig'.jsonls.setup { }
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 require 'lspconfig'.jsonls.setup {
+  capabilities = capabilities,
   commands = {
     Format = {
       function()
