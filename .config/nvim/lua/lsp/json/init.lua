fa476cc2157d6c77702ed948f2c3e6bc8af60c95
Tom Jordan <tdjordan@gmail.com>
2021-12-13 18:23:31 -0600

    turn off lsp formatting command

--- .config/nvim/lua/lsp/json/init.lua
+++ .config/nvim/lua/lsp/json/init.lua
@@ -116,17 +116,17 @@ local opts = {
       schemas = require 'schemastore'.json.schemas()
     }
   }
-  , commands = {
-    Format = {
-      function()
-        vim.lsp.buf.range_formatting(
-          {}
-          , { 0, 0 }
-          , { vim.fn.line('$'), 0 }
-        )
-      end
-    }
-  }
+  -- , commands = {
+  --   Format = {
+  --     function()
+  --       vim.lsp.buf.range_formatting(
+  --         {}
+  --         , { 0, 0 }
+  --         , { vim.fn.line('$'), 0 }
+  --       )
+  --     end
+  --   }
+  -- }
 }
 
 return opts
