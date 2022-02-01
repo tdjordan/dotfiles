e19a39a4ab700dc16dec568fcda9be5a702871c6
Tom Jordan <tdjordan@gmail.com>
2022-02-01 16:04:15 -0600

    messing around with font definitions

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,10 +1,115 @@
 local wt = require 'wezterm'
 
+local font_with_fallback = function( name, params )
+  local names = {
+    name
+    , 'Noto Color Emoji'
+    , 'JetBrains Mono'
+  }
+  return wt.font_with_fallback( names, params )
+end
+
 return {
   color_scheme = 'Batman'
-  , font = wt.font 'FiraCode Nerd Font'
-  , font_size = 17.0
+  , font_rules = {
+    ---  normal
+    ---
+    {
+      italic = false
+      -- , intensity = 'Bold'
+      , font = wt.font_with_fallback {
+        -- 'Source Code Pro',
+        -- 'SauceCodePro Nerd Font Mono',
+        -- 'NotoMono Nerd Font Mono',
+        'FiraCode Nerd Font',
+        'NotoMono Nerd Font',
+        -- 'JetBrains Mono',
+        -- 'VictorMono Nerd Font Mono',
+        'Hack Nerd Font Mono'
+        -- 'Symbols Nerd Font Mono'
+      -- , font = wt.font {
+      --   family = 'FiraCode Nerd Font',
+        -- family = 'VictorMono Nerd Font Mono',
+        -- weight = 'Regular',
+        -- stretch = 'Normal',
+        -- italic = false
+      }
+    },
+
+    ---  normal
+    ---
+    {
+      italic = false
+      , intensity = 'Bold'
+      , font = wt.font_with_fallback {
+        -- 'Hack Nerd Font Mono',
+        'VictorMono Nerd Font Mono',
+        -- 'Symbols Nerd Font Mono'
+        -- family = 'VictorMono Nerd Font Mono',
+        -- weight = 'Bold',
+        -- stretch = 'Normal',
+        -- italic = false
+      }
+    },
+
+    ---  bold
+    ---
+    -- {
+    --   italic = false
+    --   , intensity = 'Bold'
+    --   , font = font_with_fallback 'VictorMono Nerd Font Mono'
+    -- },
+
+    ---  italic
+    ---
+    -- {
+    --   italic = true
+    --   , font = wt.font(
+    --       {
+    --         family = 'VictorMono Nerd Font Mono',
+    --         italic = true
+    --       },
+    --       { family = 'Noto Color Emoji', italic = true },
+    --       { family = 'JetBrains Mono'  , italic = true }
+    --     )
+    -- },
+    -- {
+    --   italic = true,
+    --   font = wt.font( 'VictorMono Nerd Font Mono', { italic = true } )
+    -- },
+    -- {
+    --   italic = true,
+    --   font = wt.font {
+    --     family = 'VictorMono Nerd Font Mono',
+    --     italic = true
+    --   }
+    -- },
+
+    ---  bold italic
+    ---
+    -- {
+    --   italic = true
+    --   , intensity = 'Bold'
+    --   , font = font_with_fallback(
+    --     'VictorMono Nerd Font Mono',
+    --     { italic = true }
+    --   )
+    -- }
+  }
+  -- , font = wt.font {
+  --   family = 'VictorMono Nerd Font Mono',
+  --   stretch = 'Expanded',
+  --   italic = true
+  -- }
+  -- , font = wt.font {
+  --   family = 'FiraCode Nerd Font',
+  --   stretch = 'Expanded',
+  --   -- italic = true
+  -- }
+  -- , font = wt.font( 'FiraCode Nerd Font', { weight = 'Regular' } )
+  -- , font = wt.font 'JetBrains Mono'
+  -- , font = wt.font 'VictorMono Nerd Font'
+  , font_size = 18.0
   -- , enable_tab_bar = false
-  , hide_tab_bar_if_only_one_tab = true
   , window_decorations = 'RESIZE'
 }
