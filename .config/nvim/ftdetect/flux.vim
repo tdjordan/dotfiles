daf35b204da815e341a122eb9d5f7defd429366e
Tom Jordan <tdjordan@gmail.com>
2022-05-16 15:41:25 -0500

    updated changes

--- .config/nvim/ftdetect/flux.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile *.flux setfiletype flux
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,21 +10,24 @@ vim.opt.shortmess:append { I = true }
 ---
 local g = vim.g
 -- vim.opt.shadafile          = 'NONE'
-g.loaded_gzip              = false
-g.loaded_matchit           = false
--- g.loaded_matchparen        = false
-g.loaded_netrwPlugin       = false
-g.loaded_tarPlugin         = false
-g.loaded_zipPlugin         = false
+-- g.loaded_shada_plugin      = 1
+g.loaded_gzip              = 1
+g.loaded_matchit           = 1
+-- g.loaded_matchparen        = 1
+g.loaded_netrwPlugin       = 1
+g.loaded_tarPlugin         = 1
+g.loaded_zipPlugin         = 1
 -- g.loaded_node_provider     = false
 -- g.loaded_perl_provider     = false
--- g.loaded_man               = false
-g.loaded_2html_plugin      = false
-g.loaded_tutor_mode_plugin = false
-g.loaded_remote_plugins    = false
+-- g.loaded_man               = 1
+g.loaded_2html_plugin      = 1
+g.loaded_tutor_mode_plugin = 1
+g.loaded_remote_plugins    = 1
 -- g.did_indent_on            = true
--- g.did_load_filetypes       = false
-g.do_filetype_lua          = true
+g.do_filetype_lua          = 1
+g.did_load_filetypes       = 0
 -- vim.cmd "syntax off"
 -- g.syntax_on                = true
 -- g.syntax_cmd               = 'off'
+
+-- vim.cmd [[inoremap jk <esc>]]
