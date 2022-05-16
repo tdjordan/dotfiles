209b80aa04c4488137c02bfcd8d06712eb394954
Tom Jordan <tdjordan@gmail.com>
2022-05-16 16:05:06 -0500

    adapt to new neovim 0.7 APIs
    * vim.api.nvim_crate_user_command
    * load `impatient` module safely

--- .config/nvim/plugin/init_plugins.lua
+++ .config/nvim/plugin/init_plugins.lua
@@ -1,5 +1,6 @@
 local fn  = vim.fn
 local cmd = vim.cmd
+local make_command = vim.api.nvim_create_user_command
 
 local packer_installed, _ = pcall(cmd, [[packadd packer.nvim]])
 local packer_compiled_fn  = fn.stdpath('config')..'/lua/packer_compiled.lua'
@@ -10,7 +11,10 @@ _ = (not packer_installed or not packer_compiled) and require 'bootstrap'
 --   require 'bootstrap'
 -- end
 
-require 'impatient'
+local ok, _ = pcall( require, 'impatient' )
+if ok then
+  vim.notify "impatient loaded"
+end
 -- require 'impatient'.enable_profile()
 local packer_compiled_post_boot = fn.filereadable(packer_compiled_fn) == 1
 _ = packer_compiled_post_boot and require 'packer_compiled'
@@ -18,10 +22,45 @@ _ = packer_compiled_post_boot and require 'packer_compiled'
 --   require 'packer_compiled'
 -- end
 
-cmd [[ silent! command PackerSync    lua require('plugins').init_packer() require('packer').sync()           ]]
-cmd [[ silent! command PackerCompile lua require('plugins').init_packer() require('packer').compile()        ]]
+make_command( "PackerSync" , function()
+    require 'plugins'.init_packer()
+    require 'packer'.sync()
+  end
+  , {}
+)
+
+make_command( "PackerCompile" , function()
+    require 'plugins'.init_packer()
+    require 'packer'.compile()
+  end
+  , {}
+)
+
+make_command( "PackerClean" , function()
+    require 'plugins'.init_packer()
+    require 'packer'.clean()
+  end
+  , {}
+)
+
+make_command( "PackerProfile" , function()
+    require 'plugins'.init_packer()
+    require 'packer'.profile_output()
+  end
+  , {}
+)
+
+make_command( "PackerStatus" , function()
+    require 'plugins'.init_packer()
+    require 'packer'.status()
+  end
+  , {}
+)
+
+-- cmd [[ silent! command PackerSync    lua require('plugins').init_packer() require('packer').sync()           ]]
+-- cmd [[ silent! command PackerCompile lua require('plugins').init_packer() require('packer').compile()        ]]
 -- cmd [[ silent! command PackerUpdate  lua require('plugins').init_packer() require('packer').update()         ]]
 -- cmd [[ silent! command PackerInstall lua require('plugins').init_packer() require('packer').install()        ]]
-cmd [[ silent! command PackerClean   lua require('plugins').init_packer() require('packer').clean()          ]]
-cmd [[ silent! command PackerProfile lua require('plugins').init_packer() require('packer').profile_output() ]]
-cmd [[ silent! command PackerStatus  lua require('plugins').init_packer() require('packer').status()         ]]
+-- cmd [[ silent! command PackerClean   lua require('plugins').init_packer() require('packer').clean()          ]]
+-- cmd [[ silent! command PackerProfile lua require('plugins').init_packer() require('packer').profile_output() ]]
+-- cmd [[ silent! command PackerStatus  lua require('plugins').init_packer() require('packer').status()         ]]
