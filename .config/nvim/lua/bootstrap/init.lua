2e74733a46f9a3148090893915468f94633914b0
Tom Jordan <tdjordan@gmail.com>
2022-06-04 02:49:16 -0500

    refactor: move compiled packer back to plugin directory
    The expected performance improvemnet while using `impatient`
    had no effect on this module.

--- .config/nvim/lua/bootstrap/init.lua
+++ .config/nvim/lua/bootstrap/init.lua
@@ -1,16 +1,12 @@
 local fn  = vim.fn
 local cmd = vim.cmd
 
----  import options and things
----
--- require 'options'
-
 ---  bootstrap packer
 ---
 local bootstrap_packer = function()
   local install_path        = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
   local packer_installed, _ = pcall(vim.cmd, [[packadd packer.nvim]])
-  local packer_compiled_fn  = fn.stdpath('config')..'/lua/packer_compiled.lua'
+  local packer_compiled_fn  = fn.stdpath('config')..'/plugin/packer_compiled.lua'
   local packer_compiled     = fn.filereadable(packer_compiled_fn) == 1
 
   if packer_installed and packer_compiled then return end
