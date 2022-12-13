e5fd086ec95c51b7cf49dbe6d64fc8351ea8344d
Tom Jordan <tdjordan@gmail.com>
2022-12-12 23:12:59 -0600

    feat(wezterm): add base configuration

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,115 +1,139 @@
 local wt = require 'wezterm'
 
-local font_with_fallback = function( name, params )
-  local names = {
-    name
-    , 'Noto Color Emoji'
-    , 'JetBrains Mono'
-  }
-  return wt.font_with_fallback( names, params )
-end
+wt.on( 'show-font-size', function( window, _ )
+  wt.log_error( window:effective_config().font_size )
+end)
+
+-- wt.on( 'format-tab-title', function( tab, tabs, panes, config, hover, max_width )
+--   if tab.is_active then
+--     return {
+--       { Background = { Color = 'blue' } },
+--       { Text = ' ' .. tab.active_pane.title .. ' ' }
+--     }
+--   end
+--   return tab.active_pane.title
+-- end)
+
+-- local SOLID_LEFT_ARROW  = utf8.char( 0xe0b2 )
+-- local SOLID_RIGHT_ARROW = utf8.char( 0xe0b0 )
+-- local SOLID_LEFT_ARROW  = '<'
+-- local SOLID_RIGHT_ARROW = '>'
+-- local SOLID_LEFT_ARROW  = '/'
+-- local SOLID_RIGHT_ARROW = '/'
+-- local SOLID_LEFT_ARROW  = ' '
+-- local SOLID_RIGHT_ARROW = ' '
+
+-- wt.on( 'format-tab-title', function( tab, tabs, panes, config, hover, max_width )
+--   -- local edge_background = '#0b0022'
+--   -- local background = '#1b1032'
+--   -- local foreground = '#808080'
+--   local background = '#444'
+--   local foreground = '#999'
+--
+--   if tab.is_active then
+--     -- background = '#2b2042'
+--     -- foreground = '#c0c0c0'
+--     background = 'black'
+--     foreground = 'white'
+--   elseif hover then
+--     background = '#3b3852'
+--     foreground = '#909090'
+--   end
+--
+--   -- local edge_foreground = background
+--
+--   -- local title = wt.truncate_right( tab.active_pane.title, max_width - 2 )
+--   -- local title = tab.active_pane.title
+--   local title = tab.tab_index + 1 .. ' ' .. tab.active_pane.title
+--
+--   return {
+--     { Background = { Color = background } },
+--     { Foreground = { Color = foreground } },
+--     { Text = SOLID_LEFT_ARROW },
+--     -- { Background = { Color = background } },
+--     -- { Foreground = { Color = foreground } },
+--     { Text = title },
+--     -- { Background = { Color = background } },
+--     -- { Foreground = { Color = foreground } },
+--     { Text = SOLID_RIGHT_ARROW },
+--   }
+-- end)
 
 return {
-  color_scheme = 'Batman'
-  , font_rules = {
-    ---  normal
-    ---
+  adjust_window_size_when_changing_font_size = false
+  , audible_bell = 'Disabled'
+  , bold_brightens_ansi_colors = true
+  -- , cell_width = .2
+  , hide_tab_bar_if_only_one_tab = true
+  -- , color_scheme = 'Batman'
+  -- , color_scheme = 'Medallion'
+  , color_scheme = 'Gruvbox Dark'
+  -- , color_scheme = 'Brogrammer'
+  -- , color_scheme = 'Builtin Pastel Dark'
+  -- , color_scheme = 'Abernathy'
+
+  , font = wt.font_with_fallback {
     {
-      italic = false
-      -- , intensity = 'Bold'
-      , font = wt.font_with_fallback {
-        -- 'Source Code Pro',
-        -- 'SauceCodePro Nerd Font Mono',
-        -- 'NotoMono Nerd Font Mono',
-        'FiraCode Nerd Font',
-        'NotoMono Nerd Font',
-        -- 'JetBrains Mono',
-        -- 'VictorMono Nerd Font Mono',
-        'Hack Nerd Font Mono'
-        -- 'Symbols Nerd Font Mono'
-      -- , font = wt.font {
-      --   family = 'FiraCode Nerd Font',
-        -- family = 'VictorMono Nerd Font Mono',
-        -- weight = 'Regular',
-        -- stretch = 'Normal',
-        -- italic = false
-      }
-    },
-
-    ---  normal
-    ---
-    {
-      italic = false
-      , intensity = 'Bold'
-      , font = wt.font_with_fallback {
-        -- 'Hack Nerd Font Mono',
-        'VictorMono Nerd Font Mono',
-        -- 'Symbols Nerd Font Mono'
-        -- family = 'VictorMono Nerd Font Mono',
-        -- weight = 'Bold',
-        -- stretch = 'Normal',
-        -- italic = false
-      }
-    },
-
-    ---  bold
-    ---
-    -- {
-    --   italic = false
-    --   , intensity = 'Bold'
-    --   , font = font_with_fallback 'VictorMono Nerd Font Mono'
-    -- },
-
-    ---  italic
-    ---
-    -- {
-    --   italic = true
-    --   , font = wt.font(
-    --       {
-    --         family = 'VictorMono Nerd Font Mono',
-    --         italic = true
-    --       },
-    --       { family = 'Noto Color Emoji', italic = true },
-    --       { family = 'JetBrains Mono'  , italic = true }
-    --     )
-    -- },
-    -- {
-    --   italic = true,
-    --   font = wt.font( 'VictorMono Nerd Font Mono', { italic = true } )
-    -- },
-    -- {
-    --   italic = true,
-    --   font = wt.font {
-    --     family = 'VictorMono Nerd Font Mono',
-    --     italic = true
-    --   }
-    -- },
-
-    ---  bold italic
-    ---
-    -- {
-    --   italic = true
-    --   , intensity = 'Bold'
-    --   , font = font_with_fallback(
-    --     'VictorMono Nerd Font Mono',
-    --     { italic = true }
-    --   )
-    -- }
+      family = 'FiraCode Nerd Font Mono',
+      -- weight = 'Medium',
+      -- weight = 'Thin',
+      -- weight = 'ExtraLight',
+      -- weight = 'Book',
+      harfbuzz_features = { 'ss05', 'ss03', 'ss04', 'ss06' }
+    }
   }
-  -- , font = wt.font {
-  --   family = 'VictorMono Nerd Font Mono',
-  --   stretch = 'Expanded',
-  --   italic = true
-  -- }
-  -- , font = wt.font {
-  --   family = 'FiraCode Nerd Font',
-  --   stretch = 'Expanded',
-  --   -- italic = true
-  -- }
-  -- , font = wt.font( 'FiraCode Nerd Font', { weight = 'Regular' } )
-  -- , font = wt.font 'JetBrains Mono'
-  -- , font = wt.font 'VictorMono Nerd Font'
-  , font_size = 18.0
-  -- , enable_tab_bar = false
+  , font_rules = {
+    {
+      intensity = 'Bold',
+      font = wt.font(
+        'VictorMono Nerd Font',
+        {
+          italic = false,
+          weight = 'Medium'
+        }
+      )
+    },
+    {
+      italic = true,
+      font = wt.font(
+        'VictorMono Nerd Font',
+        {
+          -- italic = true,
+          style = 'Italic'
+          -- style = 'Oblique'
+        }
+      )
+    },
+    {
+      intensity = 'Bold',
+      italic = true,
+      font = wt.font(
+        'VictorMono Nerd Font',
+        {
+          italic = true,
+          weight = 'Bold'
+        }
+      )
+    }
+  }
+
+  , font_size = 17.0
+  -- , freetype_load_target = 'Mono'
+  -- , line_height = 1.1
+  , enable_tab_bar = true
+  , tab_bar_at_bottom = true
   , window_decorations = 'RESIZE'
+  -- , window_decorations = 'NONE'
+  , window_padding = {
+    left = 0
+    , right = 0
+    , top = 0
+    , bottom = 0
+  }
+  -- , enable_scroll_bar = false
+  , use_fancy_tab_bar = false
+  , term = 'wezterm'
+  , keys = {
+    { key = 'E', mods = 'CTRL', action = wt.action { EmitEvent = 'show-font-size' }}
+  }
 }
