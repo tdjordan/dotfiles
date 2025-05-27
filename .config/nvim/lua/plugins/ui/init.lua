3c31b6796ca129c4fec505e2dc521b0939e5519a
Tom Jordan <tdjordan@gmail.com>
2025-05-27 16:57:30 -0500

    feat(snacks): initial config

--- .config/nvim/lua/plugins/ui/init.lua
+++ .config/nvim/lua/plugins/ui/init.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'vigoux/notifier.nvim'
+    'tdjordan/notifier.nvim'
     , lazy = false
     , enabled = true
     , opts = {
@@ -505,5 +505,34 @@ return {
       'PulseList'
     }
     , config = true
+  },
+  {
+    "folke/snacks.nvim"
+    , priority = 1000
+    , lazy = false
+    , keys = {
+      {
+        '<leader>e', function()
+          Snacks.explorer()
+        end, desc = 'explorer'
+      }
+    }
+    , opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = false },
+      dashboard = { enabled = false },
+      explorer = { enabled = false },
+      indent = { enabled = false },
+      input = { enabled = false },
+      picker = { enabled = false },
+      notifier = { enabled = false },
+      quickfile = { enabled = false },
+      scope = { enabled = false },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false },
+      words = { enabled = false }
+    }
   }
 }
